aad4b31e7dc04402cc7ba47129e4015ab479f524
Amanda D <adnamadiaz@gmail.com>
2020-08-28 13:04:46 -0400

    Se inicia proyecto con React

---
+++ public/Firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDEYdFhJyKlHkw4fCXoOOUF6ef6z_etofg",
+    authDomain: "burger-app-e9557.firebaseapp.com",
+    databaseURL: "https://burger-app-e9557.firebaseio.com",
+    projectId: "burger-app-e9557",
+    storageBucket: "burger-app-e9557.appspot.com",
+    messagingSenderId: "1041053403407",
+    appId: "1:1041053403407:web:7ccd92c1beaf0152e1d67a",
+    measurementId: "G-Q7TXP9DDGD"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
