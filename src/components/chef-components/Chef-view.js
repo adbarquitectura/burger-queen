bec14375639d1b99e9ca43a96efecbc733169d81
SolMarina <sparraa17@gmail.com>
2020-09-03 18:47:54 -0400

    vista principal y vista del cocinero

--- src/components/chef-components/Chef-view.js
+++ src/components/chef-components/Chef-view.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import styles from './Chef.module.css';
+import logo from '../../img/logo.png';
 
 const ChefView = () => {
   return (
     <div className={styles.container}>
-      <h1>Estas en la vista de cocina</h1>
-    </div>
-  );
-}
+      <div className={styles.navBar}>
+          <div>
+            <img src={logo} className={styles.logo} alt="" />
+          </div>
+          <nav className="links">
+            <ul>
+              <li><a href="/">Menú Principal</a></li>
+              <li><a href="/">Postres</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.sectionPedido}>
+         <div className={styles.vistaPedido}>pedido</div>
+         <div className={styles.vistaPedido}>pedio</div>
+         <div className={styles.vistaPedido}>pedido</div>
+        </div>
+        <div className={styles.sectionPedidolisto}>
+        <div className={styles.verPedido}>pedido</div>
+         <div className={styles.verPedido}>pedio</div>
+         <div className={styles.verPedido}>pedido</div>
+        </div>
+        </div>
+    );
+  }
 
 export default ChefView;
